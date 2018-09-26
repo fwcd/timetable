f843593950300f2d9414d75bab47340689e52858
fwcd <fwcdmail@gmail.com>
2018-09-26 22:52:08 +0200

    Added FastisFX library (calendar widget for JavaFX)

--- src/main/java/com/fwcd/timetable/view/TimeTableAppView.java
+++ src/main/java/com/fwcd/timetable/view/TimeTableAppView.java
@@ -1,17 +1,16 @@
 package com.fwcd.timetable.view;
 
 import com.fwcd.timetable.view.utils.FxView;
+import com.jibbow.fastis.WeekView;
 
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 public class TimeTableAppView implements FxView {
 	private final Pane node;
 	
 	public TimeTableAppView() {
-		node = new Pane();
-		node.getChildren().add(new Label("Hello FX!"));
+		node = new WeekView();
 	}
 	
 	@Override
