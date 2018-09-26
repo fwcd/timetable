860d22c1687149ed6494808c7265333d5eaf6411
fwcd <fwcdmail@gmail.com>
2018-09-26 23:31:57 +0200

    Calendar view, model and viewmodel

--- src/main/java/com/fwcd/timetable/model/TimeTableAppModel.java
+++ src/main/java/com/fwcd/timetable/model/TimeTableAppModel.java
@@ -1,9 +1,22 @@
 package com.fwcd.timetable.model;
 
+import java.time.LocalDateTime;
+
+import com.fwcd.timetable.model.calendar.CalendarEvent;
 import com.fwcd.timetable.model.calendar.CalendarModel;
 
 public class TimeTableAppModel {
 	private final CalendarModel calendar = new CalendarModel();
+	
+	{
+		for (int i=0; i<24; i++) {
+			calendar.getEvents().add(new CalendarEvent.Builder("Test")
+				.start(LocalDateTime.now().plusHours(i))
+				.end(LocalDateTime.now().plusHours(i + 1))
+				.build()
+			);
+		}
+	}
 	
 	public CalendarModel getCalendar() { return calendar; }
 }
--- src/main/java/com/fwcd/timetable/model/calendar/CalendarModel.java
+++ src/main/java/com/fwcd/timetable/model/calendar/CalendarModel.java
@@ -1,5 +1,9 @@
 package com.fwcd.timetable.model.calendar;
 
+import com.fwcd.fructose.structs.ObservableList;
+
 public class CalendarModel {
+	private final ObservableList<CalendarEvent> events = new ObservableList<>();
 	
+	public ObservableList<CalendarEvent> getEvents() { return events; }
 }
