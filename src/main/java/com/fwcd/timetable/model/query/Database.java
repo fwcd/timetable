726b8c44d85ecd09a9d37ab65eea291f4efa0b94
fwcd <fwcdmail@gmail.com>
2018-10-03 12:03:22 +0200

    Made 'Query' a functional interface and removed Database interface

--- src/main/java/com/fwcd/timetable/model/query/Database.java
+++
@@ -1,5 +0,0 @@
-package com.fwcd.timetable.model.query;
-
-public interface Database {
-	Query newQuery();
-}
--- src/main/java/com/fwcd/timetable/model/query/Query.java
+++ src/main/java/com/fwcd/timetable/model/query/Query.java
@@ -1,11 +1,7 @@
 package com.fwcd.timetable.model.query;
 
-import java.util.List;
-
 import com.fwcd.fructose.Result;
 
 public interface Query {
-	List<ParameterKey> getKeys();
-	
-	Result<QueryResult, Exception> perform(List<Parameter> parameters);
+	Result<QueryResult, Exception> perform();
 }
