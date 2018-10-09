a925f95b2c236f386e6e2f228afee328e671738a
fwcd <fwcdmail@gmail.com>
2018-10-09 17:13:19 +0200

    Appointment rendering in MonthDayView

--- src/main/java/com/fwcd/timetable/view/calendar/monthview/MonthDayView.java
+++ src/main/java/com/fwcd/timetable/view/calendar/monthview/MonthDayView.java
@@ -1,20 +1,25 @@
 package com.fwcd.timetable.view.calendar.monthview;
 
 import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 import com.fwcd.fructose.structs.ObservableList;
 import com.fwcd.timetable.model.calendar.CalendarModel;
 import com.fwcd.timetable.view.utils.FxView;
 import com.fwcd.timetable.view.utils.SubscriptionStack;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class MonthDayView implements FxView, AutoCloseable {
 	private final BorderPane node;
 	private final Label indexLabel;
+	private final VBox content;
 	
 	private final ObservableList<CalendarModel> calendars;
 	private final SubscriptionStack calendarSubscriptions = new SubscriptionStack();
@@ -25,8 +30,12 @@ public class MonthDayView implements FxView, AutoCloseable {
 		node = new BorderPane();
 		
 		indexLabel = new Label(Integer.toString(date.getDayOfMonth()));
-		indexLabel.setFont(Font.font(14));
+		indexLabel.setFont(Font.font(null, FontWeight.BOLD, 14));
+		BorderPane.setAlignment(indexLabel, Pos.TOP_CENTER);
 		node.setTop(indexLabel);
+		
+		content = new VBox();
+		node.setCenter(content);
 		
 		calendars.listen(it -> updateListenersAndView());
 	}
@@ -38,7 +47,11 @@ public class MonthDayView implements FxView, AutoCloseable {
 	}
 	
 	private void updateView() {
-		// TODO
+		content.getChildren().setAll(calendars.stream()
+			.flatMap(it -> it.getAppointments().stream())
+			.map(it -> new Label(it.getName().get()))
+			.collect(Collectors.toList())
+		);
 	}
 	
 	@Override
