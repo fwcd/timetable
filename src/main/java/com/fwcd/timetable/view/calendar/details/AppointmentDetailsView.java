9dc977172ec9d118f6dd8972d15a4a5efa9d2002
fwcd <fwcdmail@gmail.com>
2018-10-10 23:15:03 +0200

    Editable appointment names

--- src/main/java/com/fwcd/timetable/view/calendar/details/AppointmentDetailsView.java
+++ src/main/java/com/fwcd/timetable/view/calendar/details/AppointmentDetailsView.java
@@ -1,17 +1,27 @@
 package com.fwcd.timetable.view.calendar.details;
 
 import com.fwcd.timetable.model.calendar.AppointmentModel;
+import com.fwcd.timetable.view.utils.FxUtils;
 import com.fwcd.timetable.view.utils.FxView;
 
+import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class AppointmentDetailsView implements FxView {
-	private final Pane node;
+	private final VBox node;
 	
 	public AppointmentDetailsView(AppointmentModel model) {
-		node = new Pane();
+		TextField title = new TextField();
+		FxUtils.bindBidirectionally(model.getName(), title.textProperty());
+		title.setFont(Font.font(14));
 		
+		node = new VBox(
+			title
+		);
+		node.setPadding(new Insets(10, 10, 10, 10));
 	}
 	
 	@Override
