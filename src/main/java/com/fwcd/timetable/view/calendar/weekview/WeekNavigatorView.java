28490ad1e080d05202326e5bdb32043e9f89ccbe
fwcd <fwcdmail@gmail.com>
2018-10-10 19:14:41 +0200

    Added start and end dates to the week navigator

--- src/main/java/com/fwcd/timetable/view/calendar/weekview/WeekNavigatorView.java
+++ src/main/java/com/fwcd/timetable/view/calendar/weekview/WeekNavigatorView.java
@@ -1,22 +1,43 @@
 package com.fwcd.timetable.view.calendar.weekview;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.fwcd.timetable.model.calendar.CalendarConstants;
 import com.fwcd.timetable.view.TimeTableAppContext;
 import com.fwcd.timetable.view.utils.FxUtils;
 import com.fwcd.timetable.view.utils.FxView;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class WeekNavigatorView implements FxView {
-	private final Pane node;
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yy"); 
+	private final HBox node;
 	
 	public WeekNavigatorView(TimeTableAppContext context, WeekContentView content) {
+		Label weekLabel = new Label();
+		weekLabel.setFont(Font.font(null, FontWeight.BOLD, 14));
+		content.getWeekStart().listenAndFire(it -> weekLabel.setText(describeWeek(it)));
+		
+		HBox.setMargin(weekLabel, new Insets(0, /* right */ 4, 0, 0));
+		
 		node = new HBox(
+			weekLabel,
 			FxUtils.buttonOf("<", content::showPreviousWeek),
 			FxUtils.buttonOf(context.localized("today"), content::showCurrentWeek),
 			FxUtils.buttonOf(">", content::showNextWeek)
 		);
+		node.setAlignment(Pos.CENTER);
+	}
+	
+	private String describeWeek(LocalDate weekStart) {
+		return FORMATTER.format(weekStart) + " - " + FORMATTER.format(weekStart.plusDays(CalendarConstants.DAYS_OF_WEEK - 1));
 	}
 	
 	@Override
