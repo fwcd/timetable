1451a5e6983e18de2cc384b897320b5871fa93f2
fwcd <fwcdmail@gmail.com>
2018-10-12 00:39:30 +0200

    Added location field to AppointmentDetailsView

--- src/main/java/com/fwcd/timetable/view/calendar/details/AppointmentDetailsView.java
+++ src/main/java/com/fwcd/timetable/view/calendar/details/AppointmentDetailsView.java
@@ -1,12 +1,15 @@
 package com.fwcd.timetable.view.calendar.details;
 
+import com.fwcd.fructose.Option;
 import com.fwcd.fructose.time.LocalDateTimeInterval;
 import com.fwcd.timetable.model.calendar.AppointmentModel;
+import com.fwcd.timetable.model.calendar.Location;
 import com.fwcd.timetable.view.utils.FxUtils;
 import com.fwcd.timetable.view.utils.FxView;
 
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -21,6 +24,15 @@ public class AppointmentDetailsView implements FxView {
 		TextField title = new TextField();
 		FxUtils.bindBidirectionally(model.getName(), title.textProperty());
 		title.setFont(Font.font(14));
+		
+		TextField location = new TextField();
+		FxUtils.bindBidirectionally(
+			model.getLocation(),
+			location.textProperty(),
+			optLocation -> optLocation.map(Location::getLabel).orElse(""),
+			newLocation -> Option.of(newLocation).filter(it -> !it.isEmpty()).map(Location::new)
+		);
+		location.setFont(Font.font(12));
 		
 		GridPane properties = new GridPane();
 		
@@ -42,8 +54,21 @@ public class AppointmentDetailsView implements FxView {
 		);
 		properties.addRow(2, new Label("End: "), end);
 		
+		TextField recurrence = new TextField();
+		FxUtils.bindBidirectionally(model.getRecurrence().getRaw(), recurrence.textProperty());
+		properties.addRow(3, new Label("Recurrence: "), recurrence);
+		
+		CheckBox ignoreDate = new CheckBox();
+		FxUtils.bindBidirectionally(model.ignoresDate(), ignoreDate.selectedProperty());
+		properties.addRow(4, new Label("Ignore Date: "), ignoreDate);
+		
+		CheckBox ignoreTime = new CheckBox();
+		FxUtils.bindBidirectionally(model.ignoresTime(), ignoreTime.selectedProperty());
+		properties.addRow(5, new Label("Ignore Time: "), ignoreTime);
+		
 		node = new VBox(
 			title,
+			location,
 			properties
 		);
 		node.setPadding(new Insets(10, 10, 10, 10));
