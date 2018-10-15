af0740fd1f5f7bf72332ee905c6a4b46c1062fb2
fwcd <fwcdmail@gmail.com>
2018-10-16 00:30:02 +0200

    Persistent settings

--- src/main/java/com/fwcd/timetable/viewmodel/TimeTableAppContext.java
+++ src/main/java/com/fwcd/timetable/viewmodel/TimeTableAppContext.java
@@ -1,28 +1,52 @@
 package com.fwcd.timetable.viewmodel;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 
 import com.fwcd.fructose.Observable;
+import com.fwcd.fructose.Option;
 import com.fwcd.fructose.ReadOnlyObservable;
 import com.fwcd.timetable.model.language.Language;
 import com.fwcd.timetable.model.language.LanguageManager;
+import com.fwcd.timetable.model.utils.GsonUtils;
 import com.fwcd.timetable.viewmodel.settings.TimeTableAppSettings;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 
 public class TimeTableAppContext {
+	private static final Gson GSON = GsonUtils.newGson();
 	private final LanguageManager languageManager = new LanguageManager();
-	private final Observable<TimeTableAppSettings> settings = new Observable<>(new TimeTableAppSettings.Builder().build());
+	
+	private final Observable<TimeTableAppSettings> settings;
 	private final Observable<Language> language = new Observable<>(new Language("", Collections.emptyMap()));
 	private final Observable<DateTimeFormatter> dateFormatter = new Observable<>(DateTimeFormatter.ISO_DATE);
 	private final Observable<DateTimeFormatter> timeFormatter = new Observable<>(DateTimeFormatter.ISO_TIME);
 	private final Observable<DateTimeFormatter> dateTimeFormatter = new Observable<>(DateTimeFormatter.ISO_DATE_TIME);
 	
+	private final Path configDirectory = Paths.get(System.getProperty("user.home"), ".timetable");
+	private final Path savedSettingsPath = configDirectory.resolve("settings.json");
+	private final boolean autoSaveSettingsEnabled = true;
+	
 	public TimeTableAppContext() {
+		settings = new Observable<>(loadSettings().orElseGet(() -> new TimeTableAppSettings.Builder().build()));
 		settings.listenAndFire(it -> {
 			languageManager.getLanguage(it.getLanguage()).ifPresent(language::set);
 			dateFormatter.set(DateTimeFormatter.ofPattern(it.getDateFormat()));
 			timeFormatter.set(DateTimeFormatter.ofPattern(it.getTimeFormat()));
 			dateTimeFormatter.set(DateTimeFormatter.ofPattern(it.getDateTimeFormat()));
+			
+			if (autoSaveSettingsEnabled) {
+				saveSettings();
+			}
 		});
 	}
 	
@@ -41,6 +65,28 @@ public class TimeTableAppContext {
 	public ReadOnlyObservable<DateTimeFormatter> getTimeFormatter() { return timeFormatter; }
 	
 	public ReadOnlyObservable<DateTimeFormatter> getDateTimeFormatter() { return dateTimeFormatter; }
+	
+	private Option<TimeTableAppSettings> loadSettings() {
+		if (Files.exists(savedSettingsPath)) {
+			try (BufferedReader reader = Files.newBufferedReader(savedSettingsPath, StandardCharsets.UTF_8)) {
+				return Option.of(GSON.fromJson(reader, TimeTableAppSettings.class));
+			} catch (JsonParseException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return Option.empty();
+	}
+	
+	private void saveSettings() {
+		try {
+			Files.createDirectories(savedSettingsPath.getParent());
+			try (BufferedWriter writer = Files.newBufferedWriter(savedSettingsPath, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+				GSON.toJson(settings.get(), writer);
+			}
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
 	
 	public String localize(String unlocalized) {
 		return language.get().localize(unlocalized);
