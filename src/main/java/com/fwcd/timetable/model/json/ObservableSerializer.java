01f5aff6601faaa143b5e830f8a628146a99b574
fwcd <fwcdmail@gmail.com>
2018-10-18 21:29:19 +0200

    Contravariant parameter bound in ObservableSerializer.serialize

--- src/main/java/com/fwcd/timetable/model/json/ObservableSerializer.java
+++ src/main/java/com/fwcd/timetable/model/json/ObservableSerializer.java
@@ -3,15 +3,16 @@ package com.fwcd.timetable.model.json;
 import java.lang.reflect.Type;
 
 import com.fwcd.fructose.Observable;
+import com.fwcd.fructose.ReadOnlyObservable;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-public class ObservableSerializer<T> implements JsonSerializer<Observable<T>>, JsonDeserializer<Observable<T>> {
+public class ObservableSerializer<T> implements JsonSerializer<ReadOnlyObservable<T>>, JsonDeserializer<Observable<T>> {
 	@Override
-	public JsonElement serialize(Observable<T> src, Type typeOfSrc, JsonSerializationContext context) {
+	public JsonElement serialize(ReadOnlyObservable<T> src, Type typeOfSrc, JsonSerializationContext context) {
 		return context.serialize(src.get());
 	}
 	
