89235e6f44f19a2b373d98b52bcccdc15397b814
fwcd <fwcdmail@gmail.com>
2018-10-21 17:50:51 +0200

    CalendarTableView data model

--- src/main/java/com/fwcd/timetable/view/calendar/listview/CalendarListView.java
+++ src/main/java/com/fwcd/timetable/view/calendar/listview/CalendarListView.java
@@ -2,8 +2,6 @@ package com.fwcd.timetable.view.calendar.listview;
 
 import java.util.stream.Collectors;
 
-import com.fwcd.fructose.structs.ObservableList;
-import com.fwcd.timetable.model.calendar.CalendarEntryModel;
 import com.fwcd.timetable.view.utils.FxNavigableView;
 import com.fwcd.timetable.view.utils.calendar.CalendarEntryListView;
 import com.fwcd.timetable.viewmodel.TimeTableAppContext;
@@ -13,25 +11,23 @@ import javafx.scene.Node;
 
 public class CalendarListView implements FxNavigableView {
 	private final Node node;
-	private final ObservableList<CalendarEntryModel> entries;
+	private final CalendarEntryListView entries;
 	
 	public CalendarListView(TimeTableAppContext context, CalendarsViewModel calendars) {
-		entries = new ObservableList<>();
+		entries = new CalendarEntryListView(context);
+		node = entries.getNode();
+		
 		calendars.getChangeListeners().add(it -> updateEntries(calendars));
 		updateEntries(calendars);
-		
-		CalendarEntryListView entriesView = new CalendarEntryListView(context);
-		entries.listenAndFire(entriesView.getNode().getItems()::setAll);
-		
-		node = entriesView.getNode();
 	}
 	
 	private void updateEntries(CalendarsViewModel calendars) {
-		entries.set(calendars.getSelectedCalendars()
+		entries.getNode().getItems().setAll(calendars.getSelectedCalendars()
 			.stream()
 			.flatMap(it -> it.getAppointments().stream())
 			.sorted()
-			.collect(Collectors.toList()));
+			.collect(Collectors.toList())
+		);
 	}
 	
 	@Override
