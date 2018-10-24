73d7e08eac10421f58107ecc4401f59fb6c7687a
fwcd <fwcdmail@gmail.com>
2018-10-24 17:18:59 +0200

    PersistentStorage abstraction

--- src/main/java/com/fwcd/timetable/viewmodel/TimeTableAppContext.java
+++ src/main/java/com/fwcd/timetable/viewmodel/TimeTableAppContext.java
@@ -1,36 +1,25 @@
 package com.fwcd.timetable.viewmodel;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 
 import com.fwcd.fructose.Observable;
-import com.fwcd.fructose.Option;
 import com.fwcd.fructose.ReadOnlyObservable;
-import com.fwcd.timetable.model.json.GsonUtils;
 import com.fwcd.timetable.model.language.Language;
 import com.fwcd.timetable.model.language.LanguageManager;
 import com.fwcd.timetable.viewmodel.settings.TimeTableAppSettings;
 import com.fwcd.timetable.viewmodel.theme.Theme;
 import com.fwcd.timetable.viewmodel.theme.ThemeManager;
 import com.fwcd.timetable.viewmodel.utils.FileSaveState;
-import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
+import com.fwcd.timetable.viewmodel.utils.PersistentStorage;
 
 public class TimeTableAppContext {
-	private static final Gson GSON = GsonUtils.newGson();
 	private final LanguageManager languageManager = new LanguageManager();
 	private final ThemeManager themeManager = new ThemeManager();
+	private final PersistentStorage persistentStorage = new PersistentStorage(Paths.get(System.getProperty("user.home"), ".timetable"));
 	
-	private final Observable<TimeTableAppSettings> settings;
+	private final Observable<TimeTableAppSettings> settings = new Observable<>(new TimeTableAppSettings.Builder().build());
 	private final Observable<Language> language = new Observable<>(new Language("", Collections.emptyMap()));
 	private final Observable<Theme> theme = new Observable<>(new Theme("", ""));
 	private final Observable<DateTimeFormatter> dateFormatter = new Observable<>(DateTimeFormatter.ISO_DATE);
@@ -39,12 +28,8 @@ public class TimeTableAppContext {
 	private final Observable<DateTimeFormatter> yearMonthFormatter = new Observable<>(DateTimeFormatter.ofPattern("MM.yyyy"));
 	
 	private final FileSaveState fileSaveState = new FileSaveState();
-	private final Path configDirectory = Paths.get(System.getProperty("user.home"), ".timetable");
-	private final Path savedSettingsPath = configDirectory.resolve("settings.json");
-	private final boolean autoSaveSettingsEnabled = true;
 	
 	public TimeTableAppContext() {
-		settings = new Observable<>(loadSettings().orElseGet(() -> new TimeTableAppSettings.Builder().build()));
 		settings.listenAndFire(it -> {
 			languageManager.getLanguage(it.getLanguage()).ifPresent(language::set);
 			themeManager.getTheme(it.getTheme()).ifPresent(theme::set);
@@ -53,11 +38,11 @@ public class TimeTableAppContext {
 			timeFormatter.set(DateTimeFormatter.ofPattern(it.getTimeFormat()));
 			dateTimeFormatter.set(DateTimeFormatter.ofPattern(it.getDateTimeFormat()));
 			yearMonthFormatter.set(DateTimeFormatter.ofPattern(it.getYearMonthFormat()));
-			
-			if (autoSaveSettingsEnabled) {
-				saveSettings();
-			}
 		});
+		
+		persistentStorage.add("settings", settings, TimeTableAppSettings.class);
+		persistentStorage.loadFromDisk();
+		persistentStorage.addAutoSaveHooks();
 	}
 	
 	public void resetSettings() { settings.set(new TimeTableAppSettings.Builder().build()); }
@@ -85,28 +70,6 @@ public class TimeTableAppContext {
 	public ReadOnlyObservable<DateTimeFormatter> getYearMonthFormatter() { return yearMonthFormatter; }
 	
 	public FileSaveState getFileSaveState() { return fileSaveState; }
-	
-	private Option<TimeTableAppSettings> loadSettings() {
-		if (Files.exists(savedSettingsPath)) {
-			try (BufferedReader reader = Files.newBufferedReader(savedSettingsPath, StandardCharsets.UTF_8)) {
-				return Option.of(GSON.fromJson(reader, TimeTableAppSettings.class));
-			} catch (JsonParseException | IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return Option.empty();
-	}
-	
-	private void saveSettings() {
-		try {
-			Files.createDirectories(savedSettingsPath.getParent());
-			try (BufferedWriter writer = Files.newBufferedWriter(savedSettingsPath, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-				GSON.toJson(settings.get(), writer);
-			}
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		}
-	}
 	
 	public String localize(String unlocalized) {
 		return language.get().localize(unlocalized);
