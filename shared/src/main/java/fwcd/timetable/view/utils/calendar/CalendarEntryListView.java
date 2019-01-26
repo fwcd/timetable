7b1137551acef0992b02dd69ac75baf37d69f73c
fwcd <fwcdmail@gmail.com>
2019-01-26 14:53:49 +0100

    WIP: Updated CalendarEntryListView

--- shared/src/main/java/fwcd/timetable/view/utils/calendar/CalendarEntryListView.java
+++ shared/src/main/java/fwcd/timetable/view/utils/calendar/CalendarEntryListView.java
@@ -3,16 +3,16 @@ package fwcd.timetable.view.utils.calendar;
 import fwcd.timetable.model.calendar.CalendarEntryModel;
 import fwcd.timetable.model.utils.Contained;
 import fwcd.timetable.view.FxView;
-import fwcd.timetable.viewmodel.TimeTableAppContext;
-
+import fwcd.timetable.viewmodel.Localizer;
+import fwcd.timetable.viewmodel.TemporalFormatters;
 import javafx.scene.control.ListView;
 
 public class CalendarEntryListView implements FxView {
 	private final ListView<Contained<CalendarEntryModel>> node;
 	
-	public CalendarEntryListView(TimeTableAppContext context) {
+	public CalendarEntryListView(Localizer localizer, TemporalFormatters formatters) {
 		node = new ListView<>();
-		node.setCellFactory(list -> new CalendarEntryListCell(context));
+		node.setCellFactory(list -> new CalendarEntryListCell(localizer, formatters));
 	}
 	
 	@Override
