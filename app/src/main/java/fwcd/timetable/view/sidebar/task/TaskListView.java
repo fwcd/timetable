9a16b53a9a8a668834ad3326315a134fbe568b38
fwcd <fwcdmail@gmail.com>
2019-09-02 01:47:30 +0200

    Update task list view

--- app/src/main/java/fwcd/timetable/view/sidebar/task/TaskListView.java
+++ app/src/main/java/fwcd/timetable/view/sidebar/task/TaskListView.java
@@ -1,11 +1,10 @@
 package fwcd.timetable.view.sidebar.task;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 import fwcd.timetable.model.calendar.CalendarEntryModel;
 import fwcd.timetable.model.calendar.CalendarEntryVisitor.TasksOnly;
-import fwcd.timetable.model.utils.Contained;
 import fwcd.timetable.view.FxView;
 import fwcd.timetable.view.utils.calendar.CalendarEntryListView;
 import fwcd.timetable.viewmodel.TimeTableAppContext;
@@ -19,14 +18,14 @@ public class TaskListView implements FxView {
 	
 	public TaskListView(TimeTableAppContext context, CalendarCrateViewModel crate, int taskListId) {
 		this.taskListId = taskListId;
-		node = new CalendarEntryListView(context.getLocalizer(), context.getFormatters()).getNode();
+		node = new CalendarEntryListView(context.getLocalizer(), context.getFormatters(), crate).getNode();
 		
 		// TODO: Update only when a task in this specific task list changes
 		updateVisibleTasks(crate.getEntries());
 		crate.getEntryListeners().add(this::updateVisibleTasks);
 	}
 	
-	private void updateVisibleTasks(List<CalendarEntryModel> allEntries) {
+	private void updateVisibleTasks(Collection<CalendarEntryModel> allEntries) {
 		node.getItems().setAll(
 			allEntries.stream()
 				.flatMap(it -> it.accept(new TasksOnly()).stream())
